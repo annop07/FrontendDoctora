5c32b742639f27c5c905e8c871ff75c475b7109c
joecsss <joecs002@gmail.com>
2025-09-20 21:07:37 +0700

    Update Intregate Login/Register 1

--- types/auth.ts
+++ types/auth.ts
@@ -1,13 +1,13 @@
-export interface LoginRequest {
-  email: string;
-  password: string;
-}
-
 export interface RegisterRequest {
   email: string;
   password: string;
   firstName: string;
   lastName: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
 }
 
 export interface LoginResponse {
