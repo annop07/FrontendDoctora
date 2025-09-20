63eaf69aa4a1051bbef14c34ce4307413b24fd89
joecsss <joecs002@gmail.com>
2025-09-20 21:07:35 +0700

    login/register

---
+++ utils/validatePass.ts
@@ -0,0 +1,11 @@
+
+export function validatePasswords(pass: string, confirm: string): string | null {
+    if (pass !== confirm) {
+      return "Passwords do not match";
+    }
+    if (pass.length < 6) {
+      return "Password must be at least 6 characters";
+    }
+    return null;
+  }
+  
