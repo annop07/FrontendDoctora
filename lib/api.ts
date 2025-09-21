fb97d1bc44d4d3e28ac11653307e61209cf8ad0e
joecsss <joecss002@gmail.com>
2025-09-21 21:06:19 +0700

    Uncomplete Admin

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 // lib/api.ts
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8082';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8083';
 
 export const API_ENDPOINTS = {
   REGISTER: `${API_BASE_URL}/api/auth/register`,
